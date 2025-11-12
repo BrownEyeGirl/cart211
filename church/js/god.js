1ec05cf699437104e2b0576a72fec0bff4089a44
Skyla Trousdale <skylatrousdale@gmail.com>
2025-11-12 13:10:03 -0500

    everything working. going to attempt popups

--- church/js/god.js
+++ church/js/god.js
@@ -5,128 +5,136 @@
 
 
 /**
+ * GOD 
  * Skyla Trousdale
- * Binary Art 
+ * 
  * 2025
  * 
  */
 
+/**
+ * Confess / Devote 
+ * 
+ * Cloud of arrows for every click 
+ * Skyla Trousdale 
+ * 
+ * 
+ */
 
-"use strict";
+// ARROW / CLOUD
+let arrow;
 
-// canvas vars 
-let img;
-let w;
-let h;
+// CLOUD
+let cloud; 
 
-// binary 
-let userText; 
-let binaryText;
-let reader; 
-//let ascii = ['0100 0001 ', '0100 0010 ', '0100 0011 ','0100 0100 ', '0100 0101	', '0100 0110 ', '0100 0111 ', '0100 1000 ', '0100 1001	', '0100 1010 ', '0100 1011 ', '0100 1100 ', '0100 1101	', '0100 1110 ', '0100 1111 ', '0101 0000 ', '0101 0001 ', '0101 0010 ', '0101 0011 ', '0101 0100 ', '0101 0101 ', '0101 0110 ', '0101 0111 ', '0101 1000 ', '0101 1001 ', '0101 1010 '];
-let alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u','v', 'w', 'x', 'y', 'z'];
+// PIXEL COLOUR MANAGERS 
+let sq; 
+let grayVal;
 
-// buttons
-let myButton; 
+// CLICKS 
+let clickCount; 
+let factor; 
 
-// preload image 
+
+
+
+/* Load Images */ 
 function preload() {
-    img = loadImage('assets/god/face.png');
+    
+
 }
 
-// Set Up Canvas, Binary Text, Image
+
 function setup() {
 
-    
+    // Load images 
+    arrow = loadImage('assets/god/cursor3.png');
+    arrow.resize(7, 0);
+    cloud = loadImage('assets/god/god2.png'); 
+    cloud.resize(0, width);
 
-    // Canvas 
-    w = (600);
-    h = (600);  
-    let cnv = createCanvas(w, h); 
-    cnv.mouseMoved(drawPixBasic);
+    cnv = createCanvas(600, 600); 
+    background(244, 253, 255); 
+    cloud.resize(0, width); 
 
-    // button 
-    //createCanvas(400, 400);
-    myButton = createButton('Click Me'); // Create the button with a label
-    myButton.position(10, 10); // Position the button on the page
+    clickCount = 0; 
+    factor = 50; 
 
-
-    // Binary Text 
-    userText = 'enter some text'; 
-    binaryText = '';
-    reader = 0; //starts at index 0 
-
-
-    // Image Configuration 
-    imageMode(CENTER);
-    noStroke();
-    background(0);
-    img.loadPixels();
-    img.filter(GRAY); // options: GREY, INVERT, THRESHOLD
-
-    // slider 
-   /* slider1 = createSlider(0, 255, 50); // starts slider at 50 
-    slider1.position(0, 200);
-    slider1.size(80);*/
 }
 
-
-// Start Project 
 function draw() {
-    // Play w Visuals 
+
+     // increase click count
+    // clickCount++; 
+    background(244, 253, 255); 
+     // draw a new click 
+    drawCloud(); 
+    //factor -=1; 
+    // frameRate(100);
 }
 
-/* VISUALIZER PLAYGROUND */ 
+function mouseClicked() {
+    clickCount++; 
 
-/* Draws Binary Text in Grid */
-function drawPixBasic() { 
-    //convertToBinary(); 
-   
-    background('#150DF7'); // resets 
-    reader = 0; 
+    if(factor > 8) {
+        factor-=1; 
+    }
+    
+}
 
-    // Generates grid by column (x) and row (y) 
-    for(let y = 10; y < h; y+=5) { // columns for pix  
-        for(let x = 0; x < w; x+=5) { // rows for pix
+function drawCloud() {
+    // sets x and y at new click clickcount
+    //factor = int(random(10, 20));
+    let vibrate = int(random(1, 3)); 
 
-            let sq = img.get(x, y); // gets colour at x,y. returns array [R, G, B, A] but since the image is black and white 
+    for(let xC = 0; xC < width; xC += factor) {
+        for(let yC = 0; yC < height; yC += factor) {
+            //x[clickCount] = xC;//int(random(0, width)); 
+           // y[clickCount] = yC;//int(random(0, height)); 
+            
+            // turn the cloud greyscale
+            sq = cloud.get(xC, yC); // gets colour at x,y. returns array [R, G, B, A] but since the image is black and white 
+            grayVal = (0.299 * sq[0]) + (0.587 * sq[1]) + (0.114 * sq[2]) // FORMULA WRITTEN BY CHATGPT, accounts for human perception of light to take RGB and turn it grayscale
+            grayVal = int(map(grayVal, 0, 300, 255, 0));
+
+            
+             tint(255, grayVal);
+             
+             image(arrow, xC+vibrate, yC+vibrate); //int(rantom(xC, xC+5)), yC);
+             //frameRate(10);
+
+           // fill(grayVal);
+            //ellipse(xC, yC, 10);
+        }
+    }
+}
+
+    //image(arrow, 5, 5);
+
+    // draw arrows at each coordinate 
+    /*for(let i = 0; i < x.length; i++) {
+       // arrow.resize(0, 50);
+       //image(arrow, x[clickCount], y[clickCount]); // draws arrow at new position
+       // console.log(x[clickCount]);
+    }*/
+
+    /*for(let xPos = 0; xPos < width; xPos+=factor) {
+        for(let yPos=0; yPos < height; yPos++) {
+
+            let sq = cloud.get(xPos, yPos); // gets colour at x,y. returns array [R, G, B, A] but since the image is black and white 
             let grayVal = (0.299 * sq[0]) + (0.587 * sq[1]) + (0.114 * sq[2]) // FORMULA WRITTEN BY CHATGPT, accounts for human perception of light to take RGB and turn it grayscale
-            
-            // sets style 
-            fill(255); //turns text blue 
-            textSize(map(grayVal, 0, 255, 1, 8));  // maps the grayscale value formula onto my text size range (5-15px)
-            
-            // loops to beginning of text string  
-           /* if(reader >= userText.length()) {
-                reader = 0; 
-            }*/
+            grayVal = int(map(grayVal, 0, 300, 10, 20));
 
-            text('x', x, y); // userText.charAt(reader)
-            //console.log(reader);
-            reader += 1;
-            //pop();  //The origin is back to (0, 0) and rotation is back to 0.
+            arrow.resize(10, 0); 
+            tint(255, grayVal);
+            image(arrow, xPos, yPos);
         }
-    }
-}
+    }*/
+ 
 
+    // cycle x, y 
+    /*for(let c = 0; c<width; c++) {
+        for(let r = 0; r<height; r++) {
 
-
-
-
-
-/* Converts User Input to Binary *
-function convertToBinary() { // Code from Javascript Docs 
-    userText = userText.toLowerCase();
-
-    for(let phrasePos = 0; phrasePos < userText.length; phrasePos++) { // cycles through each character in the phrase 
-        for(let abc = 0; abc < 26; abc++) { // cycles through abcs for each character 
-            if(userText[phrasePos] == alph[abc]) { // finds which character in the alphabet its equal to 
-                binaryText += ascii[abc];
-                //break; 
-            }
         }
-    }
-    
-    
-    console.log("Binary: " + binaryText); 
-} */
+    }*/
